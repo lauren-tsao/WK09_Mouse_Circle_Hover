1d07e1ef9e636543e298665134a10baf8e17ce64
Lauren Tsao <et2676@nyu.edu>
2024-11-04 22:14:00 -0500

    added code

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
+let xy;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255, 20, 120);
+  xy = { x: width / 2, y: height / 2, d: 50 };
 }
 
 function draw() {
+  background(255, 20, 120);
+  ellipse(mouseX + xy.x, mouseY + xy.y, xy.d);
+  xy = {
+    x: 200 * noise(frameCount / 200) - 100,
+    y: 200 * noise(frameCount / 200 + 1000) - 100,
+    d: 50,
+    // d: 80 * noise(frameCount/100 + 20202) + 10
+  };
 }
